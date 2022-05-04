e131218550d114ee0a0a4eafa1602fa0869c3647
aakash.mangla <aakash.mangla@thoughtworks.com>
2022-05-04 20:08:21 +0530

    Add. Functionality to add songs to playlist

--- src/main/java/com/thoughtworks/spotify/PlayList.java
+++ src/main/java/com/thoughtworks/spotify/PlayList.java
@@ -1,4 +1,15 @@
 package com.thoughtworks.spotify;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayList {
+    List<Song> songs = new ArrayList<>();
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public List<Song> songs() {
+        return songs;
+    }
 }
--- src/test/java/com/thoughtworks/spotify/PlaylistTest.java
+++ src/test/java/com/thoughtworks/spotify/PlaylistTest.java
@@ -2,6 +2,9 @@ package com.thoughtworks.spotify;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlaylistTest {
@@ -9,5 +12,15 @@ public class PlaylistTest {
     void canCreatePlaylist(){
         PlayList playList = new PlayList();
         assertEquals(PlayList.class,playList.getClass());
+    }
+
+    @Test
+    void canAddSongToPlaylist(){
+        PlayList playList = new PlayList();
+        Song song = new Song();
+        playList.addSong(song);
+        List<Song> expected = Arrays.asList(song);
+        List<Song> songs = playList.songs();
+        assertEquals(expected,songs);
     }
 }
